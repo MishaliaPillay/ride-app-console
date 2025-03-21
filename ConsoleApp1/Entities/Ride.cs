87a3b78260d06e268373dceac9755833ecfa2e34
Mishalia Pillay <Mishalia.Pillay@boxfusion.io>
2025-03-21 22:58:55 +0200

    initial-commit

---
+++ ConsoleApp1/Entities/Ride.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Entities
+{
+    internal class Ride
+    {
+        private Location PickUp { get; set; }
+        private Location DropOff { get; set; }
+        private Passenger Passenger { get; set; }
+        private Driver Driver { get; set; }
+        private int Rating { get; set; }
+        private decimal Rate { get; set; }
+    }
+}
