ac313bc5ad8cbe73d20ee918847ac120c0bb750a
Mishalia Pillay <Mishalia.Pillay@boxfusion.io>
2025-03-22 18:53:05 +0200

    initial-files

---
+++ Ride-app/Enities/Driver.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ride_app.Enities;
+namespace Ride_app.Enities
+{
+    internal class Driver : User
+    {
+        private bool _isAvailable { get; set; }
+        private List<Ride> _completedRides { get; set; }
+        private float _rating { get; set; }
+    }
+}
---
+++ Ride-app/Enities/Passenger.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ride_app.Enities;
+
+namespace Ride_app.Enities
+{
+    internal class Passenger : User
+    {
+        private List<Ride> _rides { get; set; }
+    }
+}
